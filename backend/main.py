af61b05ca39e7e4cf902f4bea09bfeadc451b5c3
Tony Fournales <tony.fournales@epitech.eu>
2026-01-29 20:21:06 +0100

    [+] login/register path and check are working on /backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,42 @@
-from fastapi import FastAPI
-from db.database import engine, Base
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from db.database import engine, get_db
 from db import models
+import schemas
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"status": "Backend is running and DB is connected"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/register")
+def register(user: schemas.UserAuth, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="email déjà utilisé")
+    
+    new_user = models.User(email=user.email, hashed_password=user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"status": "success", "user": new_user.email}
+
+@app.post("/login")
+def login(user: schemas.UserAuth, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email, 
+        models.User.hashed_password == user.password
+    ).first()
+    if not db_user:
+        raise HTTPException(status_code=401, detail="email ou mdp incorrect")
+    return {"status": "success", "message": "connecté !"}
---
+++ backend/schemas.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel, EmailStr
+
+class UserAuth(BaseModel):
+    email: EmailStr
+    password: str
